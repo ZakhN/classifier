c59defbe3588105e88d36b1ba3605e6c56b369dd
Nikolaj <nzaharov86@gmail.com>
2022-02-14 21:17:47 +0300

    Aded main, added test

--- main.py
+++ main.py
@@ -33,5 +33,56 @@ def load_data():
     return ds_train, ds_test, info
 
 
-# Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
+def create_model(input_shape):
+    # построение модели
+    model = Sequential()
+    model.add(Conv2D(filters=32, kernel_size=(3, 3), padding="same", input_shape=input_shape))
+    model.add(Activation("relu"))
+    model.add(Conv2D(filters=32, kernel_size=(3, 3), padding="same"))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), padding="same"))
+    model.add(Activation("relu"))
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), padding="same"))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(filters=128, kernel_size=(3, 3), padding="same"))
+    model.add(Activation("relu"))
+    model.add(Conv2D(filters=128, kernel_size=(3, 3), padding="same"))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    # сглаживание неровностей
+    model.add(Flatten())
+    # полносвязный слой
+    model.add(Dense(1024))
+    model.add(Activation("relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation="softmax"))
+    # печатаем итоговую архитектуру модели
+    model.summary()
+    # обучение модели с помощью оптимизатора Адама
+    model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+    return model
+
+
+if __name__ == "__main__":
+    # загружаем данные
+    ds_train, ds_test, info = load_data()
+    # конструируем модель
+    model = create_model(input_shape=info.features["image"].shape)
+    # несколько хороших обратных вызовов
+    logdir = os.path.join("logs", "cifar10-model-v1")
+    tensorboard = TensorBoard(log_dir=logdir)
+    # убедимся, что папка с результатами существует
+    if not os.path.isdir("results"):
+        os.mkdir("results")
+    # обучаем
+    model.fit(ds_train, epochs=epochs, validation_data=ds_test, verbose=1,
+              steps_per_epoch=info.splits["train"].num_examples // batch_size,
+              validation_steps=info.splits["test"].num_examples // batch_size,
+              callbacks=[tensorboard])
+    # сохраняем модель на диске
+    model.save("results/cifar10-model-v1.h5")
